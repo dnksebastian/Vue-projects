4f827458e64da0cbbea76c476ebaa8f93681dd4b
dnksebastian <sebastian.dulniawka@gmail.com>
2023-01-06 22:02:29 +0100

    section 8

--- Section8/section8/src/App.vue
+++ Section8/section8/src/App.vue
@@ -1,29 +1,50 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <the-header title="RememberMe"></the-header>
+  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<script>
+import StoredResources from "./components/learning-resources/StoredResources.vue";
+import TheHeader from './components/layouts/TheHeader.vue';
+
+export default {
+  components: {
+    StoredResources,
+    TheHeader,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vue.js documentation.",
+          link: "https://vuejs.org",
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn to google...",
+          link: "https://google.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+html {
+  font-family: "Roboto", sans-serif;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+body {
+  margin: 0;
 }
 </style>
--- Section8/section8/src/main.js
+++ Section8/section8/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
+
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import BaseCard from './components/UI/BaseCard.vue';
+
+const app = createApp(App);
+
+app.component('base-card', BaseCard)
+
+app.mount('#app');
